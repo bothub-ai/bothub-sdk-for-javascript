5cb7433fd1a9e68fa22d8a53d3d1201dfec90a17
赵煜杰 <825407762@qq.com>
2017-06-23 13:54:18 +0800

    Del any

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
+const es3ifyPlugin = require('es3ify-webpack-plugin');
 
 const PrepackWebpackPlugin = require('prepack-webpack-plugin').default;
 
 module.exports = {
     entry: "./index.js",
     output: {
-        path: path.resolve(__dirname, "test"),
+        path: path.resolve(__dirname, "dist"),
         filename: "bothub.js",
     },
     module: {
@@ -21,29 +22,24 @@ module.exports = {
                         'es2015',
                     ],
                     plugins: [
-                        // ["transform-runtime", {
-                        //     // "helpers": true,
-                        //     // "polyfill": true,
-                        //     "regenerator": false,
-                        //     // "moduleName": "babel-runtime"
-                        // }],
-                        'transform-es3-member-expression-literals',
-                        'transform-es3-property-literals',
-                        "transform-es5-property-mutators",
-                        ['transform-es2015-modules-commonjs', {"loose": true}],
-                        ["transform-es2015-for-of", {"loose": true}],
-                        ['transform-es3-modules-literals', {}],
+                        // 'transform-es3-member-expression-literals',
+                        // 'transform-es3-property-literals',
+                        // "transform-es5-property-mutators",
+                        // ['transform-es2015-modules-commonjs', {"loose": true}],
+                        // ["transform-es2015-for-of", {"loose": true}],
+                        // ['transform-es3-modules-literals', {}],
                     ],
                 }
             }
         }]
     },
     plugins: [
+        new es3ifyPlugin(),
         // new PrepackWebpackPlugin({}),
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false
-        //     }
-        // }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
     ]
 };

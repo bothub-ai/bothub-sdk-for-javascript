4abc46268bb67c812fd47d95eb435c3d0a6f2b11
sinkcup <sinkcup@live.it>
2017-09-18 15:22:15 +0800

    opt: follow standard. #1

--- src/Util.js
+++ src/Util.js
@@ -65,14 +65,17 @@ function getFbUserId() {
     return fb_user_id;
 }
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
+function getParameterByName(name, url = window.location.href) {
     name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
         results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+    if (!results) {
+        return null;
+    }
+    if (!results[2]) {
+        return '';
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
 export default {

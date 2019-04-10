a8ca489ea84979373c9744a933e54cd76893045c
xiaoboost <xiaoboost@qq.com>
2019-04-10 10:17:15 +0800

    update package

--- build/webpack.dev.ts
+++ build/webpack.dev.ts
@@ -1,8 +1,7 @@
 process.env.NODE_ENV = 'development';
 
-import Fs from 'fs';
+import fs from 'fs';
 import Webpack from 'webpack';
-import MemoryFS from 'memory-fs';
 import FriendlyErrorsPlugin from 'friendly-errors-webpack-plugin';
 
 import { join } from 'path';
@@ -33,57 +32,6 @@ baseConfig.plugins!.push(
 );
 
 const compiler = Webpack(baseConfig);
-const ffs = compiler.outputFileSystem = new MemoryFS();
-const indexHtml = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Bothub SDK for JavaScript</title>
-    <style>
-        html,body {
-            height: 100%;
-            min-height: 2000px;
-        }
-    </style>
-    <script>
-    if (!window.bhAsyncInit) {
-        window.bhAsyncInit = [];
-    }
-
-    // 初始化函数
-    window.bhAsyncInit.push(function() {
-        window.BOTHUB.init({
-            debug: true,
-            language: "zh_CN",
-            widgets: [
-                {
-                    id: "bothub-widget-100010-1",
-                    type: 1 /* Customerchat */,
-                    pageId: 374118179628713,
-                },
-            ],
-        });
-    });
-
-    (function(d){
-        var js, id = 'bothub-jssdk'; if (d.getElementById(id)) {return;}
-        js = d.createElement('script'); js.id = id;
-        js.async = true; js.src = "/sdk.js";
-        d.getElementsByTagName('head')[0].appendChild(js);
-    }(document));
-    </script>
-</head>
-<body>
-    <h1>Bothub SDK for JavaScript 测试页面</h1>
-</body>
-</html>`;
-
-// 首页写入内存
-ffs.mkdirpSync(output);
-ffs.writeFileSync(join(output, 'index.html'), indexHtml);
 
 compiler.watch(
     { ignored: /node_modules/ },
@@ -106,12 +54,14 @@ app.use((ctx: Context, next: Function) => {
         ? join(output, ctx.path, 'index.html')
         : join(output, ctx.path);
 
-    if (!ffs.existsSync(filePath)) {
+    if (!fs.existsSync(filePath)) {
         ctx.status = 404;
         ctx.length = 0;
         next();
         return (false);
     }
+
+    const fileStat = fs.statSync(filePath);
 
     ctx.type = getType(filePath)!;
     ctx.lastModified = new Date();
@@ -119,15 +69,15 @@ app.use((ctx: Context, next: Function) => {
     ctx.set('Accept-Ranges', 'bytes');
     ctx.set('Cache-Control', 'max-age=0');
 
-    ctx.length = Buffer.from(ffs.readFileSync(filePath)).length;
-    ctx.body = ffs.createReadStream(filePath);
+    ctx.length = fileStat.size;
+    ctx.body = fs.createReadStream(filePath);
 
     next();
 });
 
 const options = {
-    key: Fs.readFileSync(resolve('cert/key.pem')),
-    cert: Fs.readFileSync(resolve('cert/cert.pem')),
+    key: fs.readFileSync(resolve('cert/key.pem')),
+    cert: fs.readFileSync(resolve('cert/cert.pem')),
 };
 
 createServer(options, app.callback()).listen(port);

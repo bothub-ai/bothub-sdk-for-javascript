575d3043ab7ce37040ffa77b7d867ea285f0ff39
xiaoboost <xiaoboost@qq.com>
2019-05-27 11:58:36 +0800

    delete some method export

--- src/module/user/index.ts
+++ src/module/user/index.ts
@@ -1,11 +1,5 @@
 import {
-    getUserRef,
-    getFacebookUserId,
-} from './facebook';
-
-import {
     getCustomUserId,
-    getNewCustomUserId,
     changeCustomUserId,
 } from './bothub';
 
@@ -14,9 +8,6 @@ export * from './facebook';
 
 // 默认导出为对外暴露的接口
 export default {
-    getUserRef,
-    getFacebookUserId,
     getCustomUserId,
-    getNewCustomUserId,
     changeCustomUserId,
 };
